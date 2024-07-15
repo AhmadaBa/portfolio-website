ddf81008e9b45e0eea51da4cfb600c6210f883cb
AhmadaBa <65003405+AhmadaBa@users.noreply.github.com>
2024-07-14 23:38:45 -0500

    init with working navbar
    simple navbar, works on mobile/pc. still no changing in color when clicked.

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Nav from '../components/Navbar';
+
+export default function Home() {
+  return (
+    <main class="flex min-h-screen flex-col bg-dark-gray">
+      <Nav/>
+    </main>
+  );
+}
