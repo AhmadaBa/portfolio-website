480f3ce24bd764142e9391956e49d513d24f328c
AhmadaBa <65003405+AhmadaBa@users.noreply.github.com>
2024-07-15 19:34:10 -0500

    utilized globals.css, added overflow-y direction for phone scroll, removed custom tailwaint.config

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Nav from '../components/Navbar';
 
 export default function Home() {
   return (
-    <main class="flex min-h-screen flex-col bg-dark-gray">
+    <main class="flex min-h-screen flex-col">
       <Nav/>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dark-gray': '#131516',
       },
     },
   },
