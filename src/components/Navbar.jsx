d17ffaa7d3e67aa3c725e90956791ba98e0d8f6f
AhmadaBa <65003405+AhmadaBa@users.noreply.github.com>
2024-07-15 20:07:56 -0500

    Update Navbar.jsx
    added pathname, and updating icons based on which page the user is in.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,31 @@
+"use client";
 import React from 'react';
+import { usePathname } from "next/navigation";
 import { GoHome } from 'react-icons/go';
 import { MdOutlineWorkOutline, MdOutlineComputer } from "react-icons/md";
 import { BiCameraMovie } from "react-icons/bi";
 
 const Navbar = () => {
+    const pathname = usePathname();
+
     return (
         <nav class="bg-inherit fixed flex w-full border border-indigo-600">
             <div class="flex items-center justify-center mx-auto mt-10 md:mt-20 p-4 border border-orange-600">
                 <div class="flex w-auto border border-yellow-600 " id="navbar-sticky">
                     <ul class="flex flex-row flex-wrap space-x-10 p-0 mt-0 font-medium border md:space-x-16">
-                        <li class="text-white text-lg p-0 hover:scale-110 font-mono">
-                            <a href="#" class="hidden md:block md:py-2 md:px-3"><GoHome class="inline align-middle mr-2" />Home</a>                        
+                        <li className="text-lg p-0 hover:scale-110 font-mono" style={{ color: pathname === "/" ? "#FF7F50" : "white" }}>
+                            <a href="#" class="hidden md:block md:py-2 md:px-3"><GoHome class="inline align-middle mr-2" />Home</a>
                             <GoHome class="md:hidden inline align-middle mr-2" size={25} />
                         </li>
-                        <li class="text-white text-lg p-0 hover:scale-110 font-mono ">
+                        <li className="text-lg p-0 hover:scale-110 font-mono" style={{ color: pathname === "/" ? "#FF7F50" : "white" }}>
                             <a href="#" class="hidden md:block md:py-2 md:px-3"><MdOutlineWorkOutline class="inline align-middle mr-2" />Experience</a>
                             <MdOutlineWorkOutline class="md:hidden inline align-middle mr-2" size={25} />
                         </li>
-                        <li class="text-white text-lg p-0 hover:scale-110 font-mono ">
+                        <li className="text-lg p-0 hover:scale-110 font-mono" style={{ color: pathname === "/" ? "#FF7F50" : "white" }}>
                             <a href="#" class="hidden md:block md:py-2 md:px-3"><MdOutlineComputer class="inline align-middle mr-2" />Project </a>
                             <MdOutlineComputer class="md:hidden inline align-middle mr-2" size={25} />
                         </li>
-                        <li class="text-white text-lg p-0 hover:scale-110 font-mono ">
+                        <li className="text-lg p-0 hover:scale-110 font-mono" style={{ color: pathname === "/" ? "#FF7F50" : "white" }}>
                             <a href="#" class="hidden md:block md:py-2 md:px-3"><BiCameraMovie class="inline align-middle mr-2" />Movies</a>
                             <BiCameraMovie class="md:hidden inline align-middle mr-2" size={25} />
                         </li>
