b9d084915fc82494050a621fdf51056acfb5719f
AhmadaBa <65003405+AhmadaBa@users.noreply.github.com>
2024-07-17 22:50:08 -0500

    worked on text, and placement of text in homepage

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,27 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div>
-      <h1>HELLO</h1>
-    </div>
+    <main class="text-white border border-yellow flex flex-col items-center">
+      <div class="border border-blue w-5/6 h-screen  ">
+        <section>
+          <h1>Hello 👋</h1>
+          <p>I'm Ahmad Bakhit Al Fayez, a Senior Computer Science student at The University of Texas at Dallas, graduating in Fall 2024.</p>
+        </section>
+
+        <section>
+          <p>I am deeply passionate about coding and the limitless opportunities it offers in technology.</p>
+          <p>I thrive on collaborating within teams, where I can leverage collective creativity and skills to solve complex problems and develop innovative solutions.</p>
+        </section>
+
+        <section>
+          <h2>In my free time, I enjoy:</h2>
+          <ul>
+            <li>Playing chess</li>
+            <li>Watching movies and listening to music</li>
+            <li>Spending time with family and friends</li>
+          </ul>
+        </section>
+        </div>
+    </main>
   );
 }
