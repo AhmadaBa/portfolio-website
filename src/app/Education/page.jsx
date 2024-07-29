ffa62abd07dcf4d052dbbf3b9c51ae9caa556f5a
AhmadaBa <65003405+AhmadaBa@users.noreply.github.com>
2024-07-29 00:57:45 -0500

    fixed stylign and wording in multiple pages

--- src/app/Education/page.jsx
+++ src/app/Education/page.jsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Education - Ahmad B",
+};
+
 export default function Education() {
   return (
     <main className="flex flex-col items-center md:h-auto h-fit text-base md:text-lg">
@@ -6,7 +10,7 @@ export default function Education() {
           <div className="space-y-8">
             <div className="pb-4  space-y-2">
               <div className="flex items-center">
-                <img src="/logos/UTD.jpg" alt="UTD Logo" className="w-12 h-12 mr-2" />
+              <img src="/logos/UTD.jpg" alt="UTD Logo" className="w-12 h-12 mr-2 rounded-full" />
                 <div className="flex flex-col">
                   <h3 className="font-semibold">University of Texas at Dallas</h3>
                   <p className="text-gray-500 pt-1">Spring 2023 - Fall 2024</p>
@@ -14,24 +18,37 @@ export default function Education() {
               </div>
               <p className="mt-5"><strong>Degree:</strong> BS Computer Science</p>
               <p><strong>GPA:</strong> 3.8</p>
-              <p><strong>Relevant Coursework:</strong> Data Structures, Algorithms, Network Security, Machine Learning</p>
+              <p><strong>Relevant Coursework:</strong></p>
+              <ul class="list-arrow">
+                <li>Advanced Data Structures & Algorithms</li>
+                <li>Machine Learning</li>
+                <li>Computer Architecture</li>
+                <li>Database Systems</li>
+                <li>UNIX</li>
+                <li>Operating Systems</li>
+                <li>Automata Theory</li>
+              </ul>
             </div>
 
             <div className="pb-4  space-y-2">
               <div className="flex items-center">
-                <img src="/logos/Dallas-College.jpg" alt="Dallas College Logo" className="w-12 h-12 mr-2" />
+                <img src="/logos/Dallas-College.jpg" alt="Dallas College Logo" className="w-12 h-12 mr-2 rounded-full" />
                 <div className="flex flex-col">
                   <h3 className="font-semibold">Dallas College</h3>
                   <p className="text-gray-500 pt-1">Fall 2022 & Summer 2023</p>
                 </div>
               </div>
               <p className="mt-5"><strong>GPA:</strong> 4.0</p>
-              <p><strong>Relevant Coursework:</strong> Introduction to Programming, Discrete Mathematics, Database Systems</p>
+              <p><strong>Relevant Coursework:</strong></p>
+              <ul class="list-arrow">
+                <li>Linear Algebra</li>
+                <li>Discrete Mathematics</li>
+              </ul>           
             </div>
 
             <div className="pb-4 space-y-2">
               <div className="flex items-center">
-                <img src="/logos/Moraine Valley.jpg" alt="Moraine Valley Logo" className="w-12 h-12 mr-2" />
+                <img src="/logos/Moraine Valley.jpg" alt="Moraine Valley Logo" className="w-12 h-12 mr-2 rounded-full" />
                 <div className="flex flex-col">
                   <h3 className="font-semibold">Moraine Valley Community College</h3>
                   <p className="text-gray-500 pt-1">Fall 2019 - Spring 2022</p>
@@ -39,8 +56,15 @@ export default function Education() {
               </div>
               <p className="mt-5"><strong>Degree:</strong> AA General Studies</p>
               <p><strong>GPA:</strong> 3.9</p>
-              <p><strong>Relevant Coursework:</strong> Introduction to Programming, Discrete Mathematics, Database Systems</p>
-            </div>
+              <p><strong>Relevant Coursework:</strong></p>
+              <ul class="list-arrow">
+                <li>Computer Science I</li>
+                <li>Computer Science II</li>
+                <li>Calculus I</li>
+                <li>Calculus II</li>
+                <li>Calculus III</li>
+                <li>Differential Equations</li>
+              </ul>            </div>
           </div>
         </section>
       </div>
--- src/app/Experience/page.jsx
+++ src/app/Experience/page.jsx
@@ -9,45 +9,47 @@ const Experience = () => {
   return (
     <main className="flex flex-col items-center md:h-auto text-base md:text-lg">
       <div className="w-5/6 md:w-5/12 text-md flex flex-col">
-      <section className="mb-8">
-          <div className="space-y-8">
-            <div className="border-b pb-4">
+        <section className="mb-8">
+          <div className="space-y-12">
+            <div className="pb-8">
               <div className="flex justify-between items-center">
                 <div className="flex items-center">
                   <div className="bg-blue-200 p-2 rounded-full mr-4">
                     <FaBriefcase size={25} />
                   </div>
-                  <div>
-                    <h3 className="text-xl font-semibold">Software Engineering Intern</h3>
-                    <p className="text-gray-500">Mercatalyst, Carrollton, TX</p>
+                  <div class="flex flex-col  ">
+                    <h3 className="text-xl font-semibold">Software Engineer Intern</h3>
+                    <p className="text-gray-500 mt-1">Mercatalyst, Carrollton, TX</p>
+                    <span className="text-gray-500">May 2024 - Present</span>
                   </div>
                 </div>
-                <span className="text-gray-500">Jun 2024 - Present</span>
               </div>
-              <ul className="mt-2 list-disc list-inside">
-                <li>Collaborated on the development of a scalable e-commerce platform using Node.js and React.</li>
-                <li>Implemented new features and optimized existing code for performance improvements.</li>
-                <li>Participated in code reviews and contributed to team knowledge sharing sessions.</li>
+              <ul className="mt-4 list-disc list-inside">
+                <li>Rewrote legacy code from callbacks to async/await, improving performance by approximately 40%.</li>
+                <li>Developed an AI image generation bot using the OpenAI API, with robust fallback mechanisms for failure scenarios.</li>
+                <li>Authored comprehensive unit and integration tests to ensure code reliability and maintainability.</li>
+                <li>Updated ESLint rules to enforce a consistent coding style and migrated to a newer version for enhanced code quality.</li>
+                <li>Collaborated effectively with a cross-functional team, participating in code reviews and knowledge-sharing sessions.</li>
               </ul>
             </div>
 
-            <div className="border-b pb-4">
+            <div className="pb-8">
               <div className="flex justify-between items-center">
                 <div className="flex items-center">
                   <div className="bg-green-200 p-2 rounded-full mr-4">
                     <FaBriefcase size={25} />
                   </div>
-                  <div>
+                  <div class="flex flex-col w-auto">
                     <h3 className="text-xl font-semibold">Computer Science Tutor</h3>
-                    <p className="text-gray-500">University of Texas at Dallas, Richardson, TX</p>
+                    <p className="text-gray-500 mt-1">Moraine Valley Community College, Palos Hills, IL</p>
+                    <span className="text-gray-500">Jan 2020 - May 2020</span>
                   </div>
                 </div>
-                <span className="text-gray-500">Sep 2022 - May 2024</span>
               </div>
-              <ul className="mt-2 list-disc list-inside">
-                <li>Provided tutoring and academic support to students in various computer science courses.</li>
-                <li>Assisted students with understanding complex concepts and solving coding challenges.</li>
-                <li>Developed and conducted study sessions to help students prepare for exams.</li>
+              <ul className="mt-4 list-disc list-inside">
+                <li>Provided tutoring support for the Introduction to Computer Science course, with an emphasis on C++.</li>
+                <li>Successfully assisted an average of 6 students per hour during high-demand tutoring sessions.</li>
+                <li>Offered tailored guidance to students encountering complex challenges in advanced computer science topics.</li>
               </ul>
             </div>
           </div>
